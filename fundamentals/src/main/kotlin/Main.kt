911919e4fc3658038654b46fc66b42f90c5531af
ZJ <zhijietan97@gmail.com>
2023-08-27 23:32:43 +0800

    refactor: entire repository to restart learning of kotlin

---
+++ fundamentals/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args : Array<String>) {
+    println("Hello World")
+    println(5+5)
+
+    // Kotlin Variables
+    var name = "Docker"
+    var age = 13
+
+    // similar to const in javascript where it cannot be reassigned
+    val dateCreated = "2023-01-01"
+
+}
